1c0a97db8e1cae54dc5601e40ba6976242ec8871
magicmark <mark@larah.me>
2014-07-26 17:22:50 +0100

    Initial Commit

---
+++ mipango iOS App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mipango iOS App
+//
+//  Created by Mark Larah on 26/07/2014.
+//  Copyright (c) 2014 mipango. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
